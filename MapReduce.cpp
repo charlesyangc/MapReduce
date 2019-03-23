ae0782ae1e2f5b9b92eb0c48c34543767747d392
Chang Yang <changyang666@gmail.com>
2019-03-23 16:42:49 -0400

    add notify/wait for openmp

--- MapReduce.cpp
+++ MapReduce.cpp
@@ -1,4 +1,5 @@
 #include <omp.h>
+// #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +7,10 @@
 #include <string.h>
 
 #include "concurrentqueue-master/concurrentqueue.h"
+
+moodycamel::ConcurrentQueue<char *> input_file_queue;
+moodycamel::ConcurrentQueue<char *> inter_file_queue[16];
+
 
 void readfile(){
 
@@ -29,6 +34,7 @@ void loadfiles(){
     // for readdir() 
     int len;
     char * last_four; 
+    int num_files = 0;
     while ((de = readdir(dr)) != NULL){
     	// printf("%s\n", de->d_name); 
     	
@@ -37,11 +43,14 @@ void loadfiles(){
     	if (len > 4){ //length of file name should be larger than 4 (".txt")
 	    	last_four = &de->d_name[len-4];
 	    	if ( (!strcmp(last_four, ".txt")) || (!strcmp(last_four, ".TXT")) ){
-	    		printf("%s\n", de->d_name);
+	    		// printf("%s\n", de->d_name);
 	    		// push this file to work queue
+          input_file_queue.enqueue(de->d_name);
+          num_files++;
 	    	}
 	    }
     }
+    printf("in loadfiles, num_files = %d \n", num_files);
   
     closedir(dr);     
 }
@@ -58,19 +67,86 @@ int java_hashCode(const char *str) {
     return hash;
 }
 
+void map_function(){
+
+}
+
+void reduce_function(){
+
+}
+
 int main (int argc, char *argv[]) {
    int i;
-   double elapsed;
-   elapsed = omp_get_wtime();
+   double elapsed;   
 
    // master thread initialize a concurrent work queue
    // refer to https://github.com/cameron314/concurrentqueue
-   #pragma omp parallel
-   {
-   }
+   // moodycamel::ConcurrentQueue<char *> q;
+   // test queue:
+   // q.enqueue(25);
+    // q.enqueue(40);
+    // int test;
+    // q.try_dequeue(test);
+    // printf("test = %d \n", test);
+    // q.try_dequeue(test);
+    // printf("test = %d \n", test);
+    // bool found = q.try_dequeue(test);
+    // printf("found = %d \n", found);
 
-   // load all files from target directory
-   loadfiles();
 
+   // open MP version
+    elapsed = omp_get_wtime();
+    // lead files and map
+    int flag_loading_file_finished = 0;
+    int num_files = 0;
+    #pragma omp parallel
+    {
+      printf("get number of thread %d, thread rank: %d\n", omp_get_num_threads(), omp_get_thread_num());
+      #pragma omp master
+      {
+        printf("master thread is %d \n", omp_get_thread_num());
+        // master thread load file
+        loadfiles();
+        flag_loading_file_finished = 1;
+      }
+      // other thread read file
+      while(flag_loading_file_finished == 0){
+        char * file_name;
+        bool found = input_file_queue.try_dequeue(file_name);
+        if (found == 1){
+          num_files++;
+          printf("if found = %d \n", found);
+          for (i = 0; i < strlen(file_name); i++){
+            printf("%c",file_name[i]);
+          }
+          printf("\n");
+        }
+      }
+    }
+    printf("num_files = %d \n", num_files);
+    // reduce
    elapsed = omp_get_wtime() - elapsed;
+
+
+   // MPI version
+   // elapsed = MPI_Wtime();
+   //   int pid;
+   //  int numP;
+
+   //  MPI_Init(&argc, &argv);                 /* start MPI           */
+   //  MPI_Comm_size(MPI_COMM_WORLD, &numP);   /* get number of ranks */
+   //  MPI_Comm_rank(MPI_COMM_WORLD, &pid);   /* get rank            */
+   //  if (pid == 0){
+   //  printf("pid == 0 \n");    
+   //  // load all files from target directory and enqueue these files
+   //  loadfiles();
+   //  // broadcast that loadfile has finished
+   // }
+   // if (pid != 0){ // all threads with pid != 0 is map/reduce thread
+   //  // get work to read
+   //  // input_file_queue.try_dequeue();
+   // }
+   // elapsed = MPI_Wtime() - elapsed;
+
+   // pthreads version   
 }
